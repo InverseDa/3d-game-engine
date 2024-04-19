0bb90579037515fb3c98d3767661f0dc9a96dda6
InverseDa <inverse_boom@outlook.com>
2024-04-19 21:44:20 +0800

    feats: thirdparty grouped into ThiryParty/xxx

--- cmake/CompileThirdPartyLibrary.cmake
+++ cmake/CompileThirdPartyLibrary.cmake
@@ -1,2 +1,8 @@
 add_subdirectory(thirdparty/src/fmt-10.2.1)
 add_subdirectory(thirdparty/src/glfw-3.4)
+
+set_target_properties(glfw PROPERTIES FOLDER "ThirdParty/glfw")
+set_target_properties(uninstall PROPERTIES FOLDER "ThirdParty/glfw")
+set_target_properties(update_mappings PROPERTIES FOLDER "ThirdParty/glfw")
+
+set_target_properties(fmt PROPERTIES FOLDER "ThirdParty/fmt")
